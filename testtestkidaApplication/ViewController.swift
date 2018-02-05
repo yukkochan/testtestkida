5f7f500bc7b01be171210484eb07c0122ced3629
yukkochan <kk0801yk@yahoo.co.jp>
2018-02-05 17:21:04 +0900

    yuko

--- testtestkidaApplication/ViewController.swift
+++ testtestkidaApplication/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    //gogogogo
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
